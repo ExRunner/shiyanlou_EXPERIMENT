0d864d92a3c75fef9926f6721062f090dad4f1d2
wzcdyx <wzcdyx@163.com>
2017-04-27 22:01:51 +0800

    test

---
+++ maxsum.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+using namespace std;
+
+int main()
+{
+    int a[20];
+    int maxsum=0,n=0;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+	scanf("%d",&a[n]);
+    }
+    for(int i=0;i<n;i++)
+    {
+	int sum=0;
+	for(int j=i;j<n;j++)
+	{
+	sum=sum+a[j];
+	if(sum>maxsum){
+	    maxsum=sum;
+		printf("%d\n",maxsum);
+	}
+	}
+    }
+    printf("%d",maxsum);
+}
